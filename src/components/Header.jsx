f10058431447f32c7dfb2cfdeaac06d84e851496
Shabbir khan <33508921+shabbirkhan0015@users.noreply.github.com>
2024-02-09 20:32:19 +0530

    build 1
    build 1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,9 +10,9 @@ const Header = ()=>{
             <h1 className="text-xs sm:text-lg">Shabbir Khan</h1>
         </div>
         <div className="hidden justify-end sm:flex space-x-3">
-            <Link to="/" className="nav-link hover:text-orange-600">Home</Link>
+            <Link to="/PersonalWebsite" className="nav-link hover:text-orange-600">Home</Link>
             <Link to="#" className="nav-link hover:text-orange-600">About</Link>
-            <Link to="PersonalWebsite/skills/" className="nav-link hover:text-orange-600">Skills</Link>
+            <Link to="skills/" className="nav-link hover:text-orange-600">Skills</Link>
         </div>
         <div className="space-x-6 flex">
             <button className="bg-orange-500 shadow rounded-full px-3 py-2 text-xs sm:text-lg"><a href="https://drive.google.com/file/d/15NatdhwZixwMEpJ8PNFfK8NAl-WjhVbX/view?usp=drive_link" target="_blank">Download CV</a></button>
@@ -23,9 +23,9 @@ const Header = ()=>{
     </div>
     {isNavOpen && (
         <div className="basis-full sm:hidden bg-gray-100 flex flex-col items-center overflow-hidden space-y-2">
-            <Link to="/" className="w-full block border border-transparent border-b-2 hover:bg-orange-600 hover:text-white text-center">Home</Link>
+            <Link to="/PersonalWebsite" className="w-full block border border-transparent border-b-2 hover:bg-orange-600 hover:text-white text-center">Home</Link>
             <Link to="#" className="w-full block border border-transparent border-b-2 hover:bg-orange-600 hover:text-white text-center">About</Link>
-            <Link to="/PersonalWebsite/skills" className="w-full block border border-transparent border-b-2 hover:bg-orange-600 hover:text-white text-center">Skills</Link>
+            <Link to="/skills" className="w-full block border border-transparent border-b-2 hover:bg-orange-600 hover:text-white text-center">Skills</Link>
      
         </div>
       )}
